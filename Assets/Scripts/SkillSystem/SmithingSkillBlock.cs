2f95d2eb42bb4a9e50c2c33e521a723c4bb0272d
Theo <theojcrawford@gmail.com>
2020-09-09 16:20:32 +1000

    Added Smithing skill track
    This is going to be used really soon as we figure out how to do crafting

---
+++ Assets/Scripts/SkillSystem/SmithingSkillBlock.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine.UI;
+
+namespace TLY.SkillSystem
+{
+    public class SmithingSkillBlock : SkillBlock
+    {
+        public SmithingSkillBlock()
+        {
+            name = "Smithing";
+            icon = (Image)UnityEditor.AssetDatabase.LoadAssetAtPath("Asset/Art/Skill Icons/SmithingSkillIcon.png", typeof(Image));
+            descript = "The ability to craft most weapons and armour, as well as some ammunition";
+            level = 1;
+            expToLvel = 1000;
+            expPool = 0;
+            exhaust = 0;
+        }
+    }
+}
