2ffdaf39be62275397179218e126cb6c49dc64d4
Theo <theojcrawford@gmail.com>
2021-04-21 12:12:01 +1000

    SkillSystem edid
    Added a new method of holding information as well as a change that enables localization for future updates.

--- Assets/Scripts/SkillSystem/SkillBlock.cs
+++ Assets/Scripts/SkillSystem/SkillBlock.cs
@@ -4,14 +4,26 @@ namespace TLY.SkillSystem
 {
     public class SkillBlock
     {
-        public string name { get; protected set; }
-        public string descript { get; protected set; }
-        public Sprite icon { get; protected set; }
+        private const int LEVEL_CAP = 10;
+        private const int BROKEN_CAP = 20;
+
+        public int skillID { get; protected set; }
         public int level { get; protected set; }
+        public bool limitBroken { get; protected set; }
 
         public int expToLvel { get; protected set; }
         public int expPool { get; protected set; }
         public int exhaust { get; protected set; }
+
+        public SkillBlock(int SID = 0)
+        {
+            skillID = SID;
+            level = 0;
+            limitBroken = false;
+            expToLvel = 1000;
+            expPool = 0;
+            exhaust = 0;
+        }
 
         public void GainExperience(int Val)
         {
@@ -33,21 +45,9 @@ namespace TLY.SkillSystem
             level++;
             expToLvel += level * 1000;
         }
+        public void BreakTheLimit()
+        {
+            limitBroken = true;
+        }
     }
-    public enum SkillName
-    {
-        Martial,                //Join the war and fight the problems? Join the local military and help fight for your lord!
-        Brawling,               //Apply fist to face(s)
-        Thievery,               //I heard you like making money... Care if others lose a bit of coin every now and then?
-        Thaumatergy,            //Magic is an interesting topic, thus the study is required. So, how can we get it to work?
-        Faith,                  //To the divine, we give unto thee. Smite the heretics from all that was and will
-        Blacksmith,             //The forge calls, so I make it unbreaking
-        Leatherwork,            //Leathers for those who no longer need cows
-        Tailor,                 //Clothes and clothwork for those with the coin to purchase it
-        Alchemy,                //Potions and tonics for all! Well, if you need a poison on the other hand...
-        Performance,            //Poetry? Music? Go forth!
-        Mercantilism,           //I heard you like making money, thus trade away!
-        Tinker,                 //Somethings are not yet ready for this world, but these things you are putting together... they are strange, no?
-        Occult,                 //Demons, eldritch horrors, what's not to love?
-    };
 }
---
+++ Assets/Scripts/SkillSystem/SkillBlockDb.cs
@@ -0,0 +1,111 @@
+﻿using System.Data;
+using System.IO;
+using UnityEngine;
+
+
+namespace TLY.SkillSystem
+{
+    public class SkillBlockDb
+    {
+        DataTable _skillBocks;
+
+        public int GetCount => _skillBocks.Rows.Count;
+        #region Returns
+        public string SkillName(int ID) => _skillBocks.Rows.Find(ID)["Name"].ToString();
+        public string SkillDescription(int ID) => _skillBocks.Rows.Find(ID)["Description"].ToString();
+        public Texture SkillIcon(int ID) => Resources.Load<Texture>(_skillBocks.Rows.Find(ID)["Sprite Adress"].ToString());
+        #endregion
+        private void CreateSkillBlock()
+        {
+            _skillBocks = new DataTable();
+            var primaryKeys = new DataColumn[1];
+
+
+            #region ID
+            DataColumn dataColumn = new DataColumn();
+            dataColumn.ColumnName = "ID";
+            dataColumn.DataType = typeof(int);
+            dataColumn.AutoIncrement = true;
+            dataColumn.AutoIncrementSeed = 0;
+            dataColumn.AutoIncrementStep = 1;
+            dataColumn.ReadOnly = true;
+            _skillBocks.Columns.Add(dataColumn);
+            primaryKeys[0] = dataColumn;
+            #endregion
+            #region Name
+            DataColumn dataColumn1 = new DataColumn();
+            dataColumn1.ColumnName = "Name";
+            dataColumn1.DataType = typeof(string);
+            dataColumn1.Unique = true;
+            _skillBocks.Columns.Add(dataColumn1);
+            #endregion
+            #region Description
+            DataColumn dataColumn2 = new DataColumn();
+            dataColumn2.ColumnName = "Description";
+            dataColumn2.DataType = typeof(string);
+            _skillBocks.Columns.Add(dataColumn2);
+            #endregion
+            #region Sprite
+            DataColumn dataColumn3 = new DataColumn();
+            dataColumn3.ColumnName = "Sprite Adress";
+            dataColumn3.DataType = typeof(string);
+            dataColumn3.Unique = true;
+            _skillBocks.Columns.Add(dataColumn3);
+            #endregion
+            #region Type of Skill
+            DataColumn dataColumn4 = new DataColumn();
+            dataColumn4.ColumnName = "Skill Type";
+            
+            #endregion
+            #region
+            //Additional things
+            #endregion
+
+            _skillBocks.PrimaryKey = primaryKeys;
+        }
+        private void PopulateSkillBlock()
+        {
+            if (File.Exists("Assets / Database / SkillsDB.xml"))
+            {
+                using (StreamReader reader = new StreamReader("Assets / Database / SkillsDB.xml"))
+                {
+                    _skillBocks.ReadXml(reader);
+                }
+            }
+        }
+        public void SaveSkillBlock()
+        {
+            using(StreamWriter writer = new StreamWriter("Assets / Database / SkillsDB.xml"))
+            {
+                _skillBocks.WriteXml(writer);
+            }
+        }
+        public static SkillBlockDb InstantiateSkillBlock()
+        {
+            SkillBlockDb self = new SkillBlockDb();
+            self.CreateSkillBlock();
+            self.PopulateSkillBlock();
+            return self;
+        }
+        public void AddSkillBlock(string Name, string Descript, string SpriteID)
+        {
+            if (SpriteID == " " || Name == " "|| Descript == " ")
+            {
+                Debug.LogError("This has no Sprite adress");
+                return;
+            }
+            DataRow row = _skillBocks.NewRow();
+            row["Name"] = Name;
+            row["Description"] = Descript;
+            row["Sprite Adress"] = SpriteID;
+            _skillBocks.Rows.Add(row);
+        }
+        
+    }
+    public enum SkillType
+    {
+        Combat,
+        Crafting,
+        Action,
+    };
+}
