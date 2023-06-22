fefd3a92a50eb42063e9dbf18d3c5e29f988b07d
Theo <theojcrawford@gmail.com>
2023-06-22 17:19:23 +1000

    Diaglogue Writer
    Now with language selectors (this will... Oh dear god, require some writing

---
+++ Assets/Scripts/Editor/CharacterDialogueEditor.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class CharacterDialogueEditor:EditorWindow
+    {
+    public List<(byte, string)> dialogueLines;
+    private LanguageList languageState;
+
+    [MenuItem("TLOY/Dialogue DB Editor")]
+    public static void ShowWindow()
+    {
+        CharacterDialogueEditor editor = new CharacterDialogueEditor();
+        editor.Show();
+    }
+    void OnGUI()
+    {
+        languageState = (LanguageList)EditorGUILayout.EnumPopup("Language selected: ",languageState, GUILayout.ExpandWidth(false));
+        GUILayout.Label("Character Name");
+        GUILayout.TextField("");
+        //EditorGUILayout.EnumPopup
+        EditorGUILayout.IntField(0);
+        GUILayout.TextArea("");
+        if (GUILayout.Button("Submit"))
+        {
+
+        }
+    }
+    private void Sumbit()
+    {
+
+    }
+
+    private enum LanguageList
+    {
+        EN,
+        FR,
+        日本,
+    };
+}
